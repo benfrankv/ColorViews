dd60fbc41dfc3862754b3a74469b393724f287ed
“Alan <alan.corona@gonet.us>
2018-02-20 10:38:04 -0600

    add view whit green color

---
+++ ColorViews/Green.swift
@@ -0,0 +1,35 @@
+//
+//  Green.swift
+//  ColorViews
+//
+//  Created by Alan Michel Corona Rocha  on 20/02/18.
+//  Copyright © 2018 Benjamín. All rights reserved.
+//
+
+import UIKit
+
+class Green: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
